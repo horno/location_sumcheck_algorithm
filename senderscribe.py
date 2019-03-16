a948dc6d7fa9b06555857ad2198979b780397e68
horno <i.palacin.a@gmail.com>
2019-03-16 04:22:53 +0100

    CRC implemented

--- senderscribe.py
+++ senderscribe.py
@@ -1,1 +1,31 @@
 
+def extract():
+    file_o = open("enviat.txt", "r")
+    text = file_o.read()
+    file_o.close()
+    return text
+
+
+def put(data):
+    file_o = open("rebut.txt", "w")
+    file_o.write(data)
+    file_o.close()
+
+
+if __name__ == "__main__":
+    pattern = 99999
+    code_len = len(str(pattern))
+    total = 0
+    raw_data = extract()
+    counter = 0
+    for i in range(0, len(raw_data)):
+        # print(raw_data[i] + str(ord(raw_data[i])))
+        counter += 1
+        total += ord(raw_data[i])
+
+    raw_data = raw_data.rstrip("\r\n")
+    encoded_data = raw_data + "00000"
+    encoded_data = encoded_data[0:len(encoded_data)-len(str(total%pattern))] + str(total%pattern)
+    #print(encoded_data)
+    print("Counter = " + str(counter))
+    put(encoded_data)
